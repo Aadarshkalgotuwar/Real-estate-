59962d18aefe394a4bbd0ca464dd9bb8ed355079
Aadarsh kalgotuwar <90601240+Aadarshkalgotuwar@users.noreply.github.com>
2023-03-27 09:44:49 +0530

    Add files via upload

---
+++ real-estate/app.js
@@ -0,0 +1,14 @@
+function function1(){
+    alert(" You have successfully subscribed to our plan ");
+}
+
+function search(){
+    alert(" Sorry query cannot be searched");
+}
+
+function subscribe()     
+
+  function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
